e1015c06560c88bca56d206495a234febaede753
Natrix06 <nadjiblefki@gmail.com>
2026-08-22 16:17:09 +0200

    les énumérés

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -1,8 +1,12 @@
 package personnages;
 
+import objets.Equipement;
+
 public class Romain {
 	private String nom;
 	private int force;
+	private Equipement[] equipements = new Equipement[2];
+	private int nbEquipement = 0;
 
 	public Romain(String nom, int force) {
 		this.nom = nom;
@@ -46,6 +50,27 @@ public class Romain {
 
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
+	}
+
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 2:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		case 1:
+			if (equipements[0] == equipement) {
+				System.out.println("Le soldat " + nom + " possède déjà un " + equipement + " !");
+				break;
+			}
+			// Pas de break ici pour laisser passer au cas 0 si c'est un autre équipement !
+		case 0:
+			equipements[nbEquipement] = equipement;
+			nbEquipement++;
+			System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+			break;
+		default:
+			break;
+		}
 	}
 
 }
