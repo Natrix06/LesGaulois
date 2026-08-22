43cda7487d1204ee33634326ef7449833823821a
Natrix06 <nadjiblefki@gmail.com>
2026-08-22 14:19:18 +0200

    creation du village Gaulois

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -1,13 +1,20 @@
 package personnages;
 
+import village_gaulois.Village;
+
 public class Gaulois {
 	private String nom;
 	private int force;
 	private int effetPotion = 1;
+	private Village village;
 
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+	}
+
+	public void setVillage(Village village) {
+		this.village = village;
 	}
 
 	public String getNom() {
@@ -33,12 +40,27 @@ public class Gaulois {
 
 	public void frapper(Romain romain) {
 		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
-		
-		romain.recevoirCoup((force*effetPotion) / 3);
+
+		romain.recevoirCoup((force * effetPotion) / 3);
 	}
 
 	public void boirePotion(int forcePotion) {
 		this.effetPotion = forcePotion;
+	}
+
+	@Override
+	public String toString() {
+		return nom;
+	}
+
+	public void sePresenter() {
+		if (village != null && this == village.getChef()) {
+			parler("Bonjour, je m'appelle " + nom + ". Je suis le chef du village " + village.getNom() + ".");
+		} else if (village != null) {
+			parler("Bonjour, je m'appelle " + nom + ". J'habite le village " + village.getNom() + ".");
+		} else {
+			parler("Bonjour, je m'appelle " + nom + ". Je voyage de villages en villages.");
+		}
 	}
 
 }
---
+++ src/village_gaulois/Village.java
@@ -0,0 +1,95 @@
+package village_gaulois;
+
+import personnages.Gaulois;
+
+public class Village {
+	private String nom;
+	private int nbVillageois = 0;
+	private Gaulois chef;
+	private Gaulois[] villageois;
+
+	public Village(String nom, Gaulois chef, final int NB_VILLAGEOIS_MAX) {
+		this.nom = nom;
+		this.chef = chef;
+		villageois = new Gaulois[NB_VILLAGEOIS_MAX];
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getNbVillageois() {
+		return nbVillageois;
+	}
+
+	public Gaulois getChef() {
+		return chef;
+	}
+
+	public void ajouterVillageois(Gaulois gaulois) {
+		villageois[nbVillageois] = gaulois;
+		nbVillageois++;
+		gaulois.setVillage(this);
+	}
+
+	public Gaulois trouverVillageois(int numVillageois) {
+		if (numVillageois > nbVillageois || numVillageois < 1) {
+			System.out.println("Il n’y a pas autant d’habitants dans notre village !");
+			return null;
+		} else {
+			return villageois[numVillageois - 1];
+		}
+	}
+
+	public static void main(String[] args) {
+		Gaulois abraracourcix = new Gaulois("Abracourix", 6);
+		Village village = new Village("Village des Irreductibles", abraracourcix, 30);
+		village.trouverVillageois(30);
+		Gaulois asterix = new Gaulois("Asterix", 8);
+		village.ajouterVillageois(asterix);
+		Gaulois obelix = new Gaulois("Obelix", 25);
+		village.ajouterVillageois(obelix);
+		Gaulois doublepolemix = new Gaulois("Doublepoemix", 4);
+		Gaulois gaulois = village.trouverVillageois(1);
+		System.out.println(gaulois);
+		gaulois = village.trouverVillageois(2);
+		System.out.println(gaulois);
+		village.afficherVillage();
+		asterix.sePresenter();
+		obelix.sePresenter();
+		doublepolemix.sePresenter();
+	}
+	
+	public void afficherVillage() {
+		System.out.println("Dans le village " + "\"" + getNom() + "\"" + " du chef " + getChef() + " vivent les legendaires gaulois :");
+		
+		for (int i = 0; i < nbVillageois; i++) {
+			if (villageois[i]!=null) {
+				System.out.println("- " + villageois[i]);
+			}
+			
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
